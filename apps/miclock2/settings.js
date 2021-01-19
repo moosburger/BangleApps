c686974acff27b232307cf7487a4aa655e38cac2
moosburger <moosburger@freenet.de>
2021-01-19 14:10:53 +0100

    Zeilende auf Windows

--- apps/miclock2/settings.js
+++ apps/miclock2/settings.js
@@ -7,7 +7,15 @@
   function getSettings(){
     return {
       cDebug: false,
-      cSize: 2
+      cSize: 2,
+      cMoveCenter: 20,
+      cShowRAM: false,
+      cMaxTime: 1100,
+      cMinTime: 240,
+      cStepThreshold: 30,
+      cIntervalResetActive: 30000,
+      cStepSensitivity: 80,
+      cStepGoal: 10000
     };
   }
 
@@ -35,10 +43,45 @@
       min: 0, max: 2, step: 1,
       onchange : saveChange('cSize')
     },
-    "Debug" : {
-      value : settings.cDebug,
+    "Center" : {
+      value : settings.cMoveCenter,
+      min : 0, max : 30, step : 1,
+      onchange : saveChange('cMoveCenter')
+    },
+    "Show RAM": {
+      value : settings.cShowRAM,
       format : v => v?"On":"Off",
-      onchange : saveChange('cDebug')
+      onchange : saveChange('cShowRAM')
+    },
+    "Max time (ms)": {
+      value : settings.cMaxTime,
+      min : 0, max : 10000, step : 100,
+      onchange : saveChange('cMaxTime')
+    },
+    "Min time (ms)": {
+      value : settings.cMinTime,
+      min : 0, max : 500, step : 10,
+      onchange : saveChange('cMinTime')
+    },
+    "Step threshold": {
+      value : settings.sCtepThreshold,
+      min : 0, max : 100, step : 1,
+      onchange : saveChange('cStepThreshold')
+    },
+    "Act.Res. (ms)": {
+      value : settings.cIntervalResetActive,
+      min : 100, max : 100000, step : 1000,
+      onchange : saveChange('cIntervalResetActive')
+    },
+    "Step sens.": {
+      value : settings.cStepSensitivity,
+      min : 0, max : 1000, step : 10,
+      onchange : saveChange('cStepSensitivity')
+    },
+    "Step goal": {
+      value : settings.cStepGoal,
+      min : 1000, max : 100000, step : 1000,
+      onchange : saveChange('cStepGoal')
     },
     '< Back': back
   });
