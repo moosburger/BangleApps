d18ed108dd14de15ad5996c019087880e5a021dc
moosburger <moosburger@freenet.de>
2021-01-20 12:24:32 +0100

    Rückbau

--- apps/miclock2/clock-mixed.js
+++ apps/miclock2/clock-mixed.js
@@ -27,27 +27,21 @@ require("Font7x11Numeric7Seg").add(Graphics);
 const Storage = require("Storage");
 const filename = 'miclock2.json';
 let settings = Storage.readJSON(filename,1) || {
-  cDebug: true,
-  cSize: 0,
-  cDiameter : 23,
-  cMoveCenter : -24,
-  cShowRAM: false,
-  cMaxTime: 1100,
-  cMinTime: 240,
-  cStepThreshold: 30,
-  cIntervalResetActive: 30000,
-  cStepSensitivity: 80,
-  cStepGoal: 10000
+  cSize: 1,
+  cDiameter : 14,
+  cShowRAM: true
 };
 
 /**************************************************************************************************
 * Defines
 * jshint esversion: 6
 **************************************************************************************************/
-const Radius = { "center": 6, "hour": 60 + settings.cDiameter, "min": 80 + settings.cDiameter, "sec" : 85 + settings.cDiameter, "dots": 92 + settings.cDiameter };
-const Center = { "x": 120, "y": 120};
+const Radius = { "center": 6, "hour": 60 + settings.cDiameter, "min": 80 + settings.cDiameter, "sec" : 85 + settings.cDiameter, "dots": 93 + settings.cDiameter };
+const Center = { "x": 120, "y": 108};
+//const Center = { "x": 120, "y": 96 };
 const Widths = { hour: 2, minute: 2, second: 1 };
-var buf = Graphics.createArrayBuffer(240,240,1,{msb:true});
+var buf = Graphics.createArrayBuffer(240,216,1,{msb:true});
+//var buf = Graphics.createArrayBuffer(240,192,1,{msb:true});
 
 const CHARGING = -1;//0x0007E0;
 const CLOCK = -1; // always white
@@ -59,61 +53,10 @@ const BTDIS = 0x000000;
 /**************************************************************************************************
 * Variablen
 **************************************************************************************************/
-var img_bt = E.toArrayBuffer(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="));
-var img_Bat = E.toArrayBuffer(atob("DhgBHOBzgc4HOP////////////////////3/4HgB4AeAHgB4AeAHgB4AeAHg"));
-var img_Pedo = E.toArrayBuffer(atob("CgoCLguH9f2/7+v6/79f56CtAAAD9fw/n8Hx9A=="));
-
-var steps = 0; //steps taken
 
 /**************************************************************************************************
 * Funktionen
 **************************************************************************************************/
-
-//*************************************************************************************************
-// FunktionsName:   drawBT
-/// \details
-/// \param[in]      -
-/// \return         -
-//*************************************************************************************************
-function drawBT() {
-  var isConnected = true;
-  var ox = 108;
-  var oy = 28;
-
-  if (settings.cDebug == false)
-    isConnected = (NRF.getSecurityStatus().connected);
-
-  if (isConnected == true)
-    buf.setColor(BTCON);
-  else
-    buf.setColor(BTDIS);
-  buf.drawImage(img_bt, Center.x + ox, Center.y - Radius.dots + oy);
-}
-
-//*************************************************************************************************
-// FunktionsName:   drawBatt
-/// \details
-/// \param[in]      -
-/// \return         -
-//*************************************************************************************************
-function drawBatt() {
-
-  var w = 30;
-  var h = 16;
-  var ox = 85;
-  var oy = 26;
-
-  if ((Bangle.isCharging()) || (settings.cDebug == true)) {
-    buf.setColor(CHARGING).drawImage(img_Bat, Center.x + ox - 16, Center.y + Radius.dots - 14);
-  }
-
-  buf.setColor(CLOCK);
-  buf.fillRect (Center.x + ox, Center.y + Radius.dots - oy, Center.x + w + ox, Center.y + Radius.dots -oy + 16);
-  buf.clearRect(Center.x + ox + 2, Center.y + Radius.dots - oy + 2, Center.x + ox + w - 2, Center.y + Radius.dots + h - oy - 2);
-  buf.fillRect (Center.x + ox + w, Center.y + Radius.dots - oy + 4, Center.x + ox + w + 4, Center.y + Radius.dots + h - oy - 4);
-  buf.setColor(CHARGING).fillRect(Center.x + ox + 2, Center.y + Radius.dots - oy + 2, Center.x + ox + 2 + E.getBattery()*(w-4)/100,Center.y + Radius.dots + h - oy - 2);
-  buf.setColor(-1);
-}
 
 //*************************************************************************************************
 // FunktionsName:   showRAMUsage
@@ -127,84 +70,6 @@ function showRAMUsage() {
     var pc = Math.round(m.usage*100/m.total);
     buf.drawString(pc+"%", Center.x + 5, Center.y - 40, true);
 }
-
-//*************************************************************************************************
-// FunktionsName:   setStepSensitivity
-/// \details
-/// \param[in]      -
-/// \return         -
-//*************************************************************************************************
-function setStepSensitivity(s) {
-  function sqr(x) { return x*x; }
-  var X=sqr(8192-s);
-  var Y=sqr(8192+s);
-  Bangle.setOptions({stepCounterThresholdLow:X,stepCounterThresholdHigh:Y});
-}
-
-//*************************************************************************************************
-// FunktionsName:   drawPEDO
-/// \details
-/// \param[in]      -
-/// \return         -
-//*************************************************************************************************
-function drawPEDO() {
-
-  var fx = 0;
-
-  buf.setFont("7x11Numeric7Seg", 1);
-  buf.setFontAlign(-1, 0);
-  if (steps < 10)
-    fx = 48;
-  else if (steps < 100)
-    fx = 36;
-  else if (steps < 1000)
-    fx = 24;
-  else if (steps < 10000)
-    fx = 12;
-
-  if (steps >= settings.cStepGoal)
-    buf.setColor(CHARGING);
-
-  //g.drawImage(img_Pedo, Center.x - 118, Center.y - Radius.dots - 1);
-  //g.drawString(steps, Center.x - 105 + fx, Center.y - Radius.dots + 4);
-  //g.drawImage(img_Pedo, Center.x - 116, Center.y - Radius.dots + 16);
-
-  buf.drawImage(img_Pedo, Center.x - 95, Center.y + Radius.dots - 32);
-  buf.drawString(steps, Center.x - 120 + fx, Center.y + Radius.dots - 14);
-}
-
-//*************************************************************************************************
-// FunktionsName:   calcSteps
-/// \details
-/// \param[in]      -
-/// \return         -
-//*************************************************************************************************
-function calcSteps() {
-    stopTimeStep = new Date(); //stop time after each step
-    stepTimeDiff = stopTimeStep - startTimeStep; //time between steps in milliseconds
-    startTimeStep = new Date(); //start time again
-
-    //Remove step if time between first and second step is too long
-    if (stepTimeDiff >= setting('cMaxTime')) { //milliseconds
-      steps--;
-    }
-    //Remove step if time between first and second step is too short
-    if (stepTimeDiff <= setting('cMinTime')) { //milliseconds
-      steps--;
-    }
-
-    //Step threshold reached
-    if (steps >= setting('cStepThreshold')) {
-      if (active == 0) {
-        stepsCounted = stepsCounted + (setting('cStepThreshold') -1) ; //count steps needed to reach active status, last step is counted anyway, so treshold -1
-        stepsOutsideTime = stepsOutsideTime - 10; //substract steps needed to reach active status
-      }
-      active = 1;
-      clearInterval(timerResetActive); //stop timer which resets active
-      timerResetActive = setInterval(resetActive, setting('cIntervalResetActive')); //reset active after timer runs out
-      steps = 0;
-    }
-  }
 
 //*************************************************************************************************
 // FunktionsName:   rotatePoint
@@ -278,41 +143,36 @@ function drawMixedClock(force) {
 
     buf.setColor(CLOCK);
     // draw date
-    buf.setFont("7x11Numeric7Seg", 3);
+    buf.setFont("7x11Numeric7Seg", 2);
     buf.setFontAlign(0, 0);
-    //Wochentag
-    //buf.drawString(locale.dow(date,true), Center.x - 116, Center.y + Radius.dots - 26, true);
-    //Tag
-    //buf.drawString((dateArray[2]), Center.x - 116, Center.y + Radius.dots - 6, true);
-    //buf.setFontAlign(1, 0);
-    //Monat
-    //buf.drawString(locale.month(date,true), Center.x + 116, Center.y + Radius.dots - 26, true);
-    //Jahr
-    //buf.drawString(dateArray[3], Center.x + 116, Center.y + Radius.dots - 6, true);
-
-    //Tag
-    buf.drawString((dateArray[2]), Center.x + 75, Center.y, true);
-    buf.setFont("6x8", 2);
+    buf.drawString((dateArray[2]), Center.x + Radius.dots - 25, Center.y, true);
 
     // draw hour and minute dots
-    for (i = 0; i < 60; i++) {
-        radius = (i % 5) ? 4 : 0;
-        //Punkte
-        point = rotatePoint(0, Radius.dots, i * 30); // 30 = Minuten; 6 = Sekunden
-        buf.fillCircle(point[0], point[1], radius);
-        //Linien
-        point = rotatePoint(0, Radius.dots, i * 6);    // 30 = Minuten; 6 = Sekunden
-        start = rotatePoint(0, Radius.dots - 5, i * 6);// 30 = Minuten; 6 = Sekunden
-        buf.drawLine(start[0], start[1], point[0], point[1]);
-        buf.fillPoly(setLineWidth(start[0], start[1], point[0], point[1], 1));
+    for (i = 1; i < 61; i++) {
+        if ((i % 5 == 0) && (i != 15))
+        { //Punkte
+          point = rotatePoint(0, Radius.dots -3, i * 6);
+          buf.fillCircle(point[0], point[1], 2);
+          buf.setFont("7x11Numeric7Seg", 2);
+          point = rotatePoint(0, Radius.dots - 20, i * 6);
+          buf.drawString(i/5 , point[0], point[1], true);
+        }
+        else
+        {
+          //Linien
+          point = rotatePoint(0, Radius.dots, i * 6);
+          start = rotatePoint(0, Radius.dots - 4, i * 6);
+          buf.drawLine(start[0], start[1], point[0], point[1]);
+          buf.fillPoly(setLineWidth(start[0], start[1], point[0], point[1], 1));
+        }
     }
 
     // draw digital time
     if(settings.cSize > 0)
     {
-      buf.setFont("6x8", settings.cSize + 1);
+      buf.setFont("7x11Numeric7Seg", settings.cSize + 1);
       buf.setFontAlign(0, 0);
-      buf.drawString(dateArray[4], Center.x, Center.y, true);
+      buf.drawString(dateArray[4], Center.x, Center.y + Radius.dots/3, true);
     }
 
     // draw new second hand
@@ -333,14 +193,20 @@ function drawMixedClock(force) {
     // draw center
     buf.fillCircle(Center.x, Center.y, Radius.center);
 
-    if((settings.cShowRAM == true) || (settings.cDebug == true))
+    if(settings.cShowRAM == true)
+    {
+      buf.setFont("6x8", 2);
       showRAMUsage();
+      buf.setFont("7x11Numeric7Seg", 1);
+    }
+    g.drawImage({width:buf.getWidth(),height:buf.getHeight(),bpp:1,buffer:buf.buffer}, 0, 38 - settings.cDiameter);
 
-    drawBT();
-    drawBatt();
-    drawPEDO();
-
-    g.drawImage({width:buf.getWidth(),height:buf.getHeight(),bpp:1,buffer:buf.buffer},0,24 + settings.cMoveCenter);
+    g.setColor(0x0F00);
+    g.drawLine(1, 120 + settings.cDiameter - 2, 240, 120 + settings.cDiameter- 2);
+    g.drawLine(120, 1, 120, 240);
+    g.setColor(0xF000);
+    g.drawLine(1, 120, 240, 120);
+    g.drawLine(1, 24, 238, 24);
   }
 }
 
@@ -351,15 +217,14 @@ function drawMixedClock(force) {
 /// \return         -
 //*************************************************************************************************
 Bangle.on('lcdPower', function(on) {
-  if (on)
+  if (on){
     drawMixedClock(true);
     Bangle.drawWidgets();
+  }
 });
 
-//When Step is registered by firmware
-Bangle.on('step', (up) => {
-  steps++; //increase step count
-  calcSteps();
+Bangle.on('touch', function(button) {
+  Bangle.showLauncher();
 });
 
 //*************************************************************************************************
@@ -376,8 +241,6 @@ Bangle.drawWidgets();
 drawMixedClock(); // immediately draw
 setInterval(drawMixedClock, 500); // update twice a second
 
-setStepSensitivity(settings.cStepSensitivity); //set step sensitivity (80 is standard, 400 is muss less sensitive)
-
 // Show launcher when middle button pressed after freeing memory first
-//setWatch(() => {Bangle.showLauncher()}, BTN2, {repeat:false,edge:"falling"});
 setWatch(() => {delete buf.buffer; Bangle.showLauncher()}, BTN2, {repeat:false,edge:"falling"});
+
--- apps/miclock2/settings.js
+++ apps/miclock2/settings.js
@@ -6,17 +6,9 @@
 
   function getSettings(){
     return {
-      cDebug: false,
       cSize: 0,
       cDiameter : 23,
-      cMoveCenter : -24,
-      cShowRAM: false,
-      cMaxTime: 1100,
-      cMinTime: 240,
-      cStepThreshold: 30,
-      cIntervalResetActive: 30000,
-      cStepSensitivity: 80,
-      cStepGoal: 10000
+      cShowRAM: false
     };
   }
 
@@ -44,14 +36,9 @@
       min: 0, max: 2, step: 1,
       onchange : saveChange('cSize')
     },
-    "Center" : {
-      value : settings.cMoveCenter,
-      min : -30, max : 30, step : 1,
-      onchange : saveChange('cMoveCenter')
-    },
     "Diameter" : {
       value : settings.cDiameter,
-      min : -30, max : 30, step : 1,
+      min : -10, max : 30, step : 1,
       onchange : saveChange('cDiameter')
     },
     "Show RAM": {
@@ -59,36 +46,6 @@
       format : v => v?"On":"Off",
       onchange : saveChange('cShowRAM')
     },
-/*    "Max time (ms)": {
-      value : settings.cMaxTime,
-      min : 0, max : 10000, step : 100,
-      onchange : saveChange('cMaxTime')
-    },
-    "Min time (ms)": {
-      value : settings.cMinTime,
-      min : 0, max : 500, step : 10,
-      onchange : saveChange('cMinTime')
-    },
-    "Step threshold": {
-      value : settings.cStepThreshold,
-      min : 0, max : 100, step : 1,
-      onchange : saveChange('cStepThreshold')
-    },
-    "Act.Res. (ms)": {
-      value : settings.cIntervalResetActive,
-      min : 100, max : 100000, step : 1000,
-      onchange : saveChange('cIntervalResetActive')
-    },
-    "Step sens.": {
-      value : settings.cStepSensitivity,
-      min : 0, max : 1000, step : 10,
-      onchange : saveChange('cStepSensitivity')
-    },
-    "Step goal": {
-      value : settings.cStepGoal,
-      min : 1000, max : 100000, step : 1000,
-      onchange : saveChange('cStepGoal')
-    },*/
     '< Back': back
   });
 });
