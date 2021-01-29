25819e14861e078442259bca48a132e920623b90
moosburger <moosburger@freenet.de>
2021-01-29 09:49:15 +0100

    size korrekted

--- apps/about/app.js
+++ apps/about/app.js
@@ -16,7 +16,7 @@ function getVersion(name,file) {
   var v = ("object"==typeof j)?j.version:false;
   //g.drawString(v?(name+" "+(v?"v"+v:"Unknown")):"NO "+name,0,y+=h);
   y += (v? h:0);
-  g.drawString(v?(name+": "+(v?"v"+v:"Unknown")):'',90,y);
+  g.drawString(v?(name+": "+(v?"v"+v:"Unknown")):'',110,y);
 }
 y+=h;
 g.setFontAlign(1,0);
